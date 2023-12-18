3b16c089ff542fa51a6d876abb77533c350bab73
Baby_shakur <147159662+KYKLA231@users.noreply.github.com>
2023-12-18 19:44:12 +0300

    Update settings.py

--- sigma/boob/settings.py
+++ sigma/boob/settings.py
@@ -1,11 +1,10 @@
 import os
-from dotenv import load_dotenv
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-load_dotenv()
+
 SECRET_KEY = os.getenv('KEY')
 
 DEBUG = True
